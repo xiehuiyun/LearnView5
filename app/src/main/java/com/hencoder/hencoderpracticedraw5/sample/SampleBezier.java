01e418e571bcc3fae23fb2647e5eb5037b0a9e0e
NataliePortman <1401010800@qq.com>
2017-08-28 11:23:37 +0800

    初始化

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/sample/SampleBezier.java
@@ -0,0 +1,93 @@
+package com.hencoder.hencoderpracticedraw5.sample;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/8/28.
+ */
+
+public class SampleBezier extends View {
+
+    private Paint mPaint;
+    private PointF start, end, control;
+    private int centerX, centerY;
+
+    public SampleBezier(Context context) {
+        super(context);
+    }
+
+    public SampleBezier(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setTextSize(60);
+
+        start = new PointF(0, 0);
+        end = new PointF(0, 0);
+        control = new PointF(0, 0);
+    }
+
+    public SampleBezier(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = w / 2;
+        centerY = h / 2;
+
+        //初始化数据点和控制点
+        start.x = centerX - 200;
+        start.y = centerY;
+        end.x = centerX + 200;
+        end.y = centerY;
+        control.x = centerX;
+        control.y = centerY - 100;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        control.x = event.getX();
+        control.y = event.getY();
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStrokeWidth(20);
+        // 绘制数据点和控制点
+        canvas.drawPoint(start.x, start.y, mPaint);
+        canvas.drawPoint(end.x, end.y, mPaint);
+        canvas.drawPoint(control.x, control.y, mPaint);
+
+        //绘制辅助线
+        mPaint.setStrokeWidth(4);
+        canvas.drawLine(start.x, start.y, control.x, control.y, mPaint);
+        canvas.drawLine(end.x, end.y, control.x, control.y, mPaint);
+        // 绘制贝塞尔曲线
+        mPaint.setColor(Color.RED);
+        mPaint.setStrokeWidth(8);
+
+        Path path = new Path();
+
+        path.moveTo(start.x, start.y);
+        path.quadTo(control.x, control.y, end.x, end.y);
+        canvas.drawPath(path, mPaint);
+
+    }
+}
